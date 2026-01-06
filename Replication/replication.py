850efc5c5bc1ab0006835c26f179da52788c74c1
rubenvdz <71780895+rubenvdz@users.noreply.github.com>
2026-01-06 23:56:43 +0100

    Confidence elicitation

--- Replication/replication.py
+++ Replication/replication.py
@@ -9,24 +9,35 @@ import torch
 import pandas as pd
 import os
 from llama_cpp import Llama
-
+import math
 
 llm = Llama.from_pretrained(
 	repo_id="bartowski/Meta-Llama-3.1-8B-Instruct-GGUF",
 	filename="Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf",
     verbose=False,
     logits_all=True,
-    temperature=0
 )
+
+prompt = """The capital of france is Paris
+Answer in the following format:
+Explanation: <free text>
+Label: <TRUE | FALSE>
+"""
 
 response = llm.create_chat_completion(
 	messages = [
 		{
 			"role": "user",
-			"content": "What is the capital of France?"
+			"content": prompt
 		}
 	],
     logprobs=True,
     top_logprobs=5,
+    temperature=0,
 )
-print(response)
+print("Response:")
+print(response['choices'][0]['message']['content'])
+print("Label:")
+print(response['choices'][0]['logprobs']['content'][-1]['token'])
+print("Estimated confidence (key token probability): ")
+print(math.exp(response['choices'][0]['logprobs']['content'][-1]['logprob']))
